4e8368da2fda5c4031357b41e5d3f35e5adf6625
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-24 10:05:24 +0100

    parallex

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,27 +2,41 @@
 
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import React from "react";
+import React, { useRef } from "react";
+import { ScrollParallax } from "react-just-parallax";
+import heroIcons from "../../public/assests/4-small.png";
+import Image from "next/image";
+import { BackgroundCircles } from "./hero/Hero";
 
 const Hero = () => {
+  const parallaxRef = useRef(null);
   useGSAP(() => {
     gsap.to("#title", { opacity: 1, y: 0, delay: 1 });
     gsap.to("#desc", { opacity: 1, y: 0, delay: 1.5 });
   }, []);
   return (
     <section className="w-full nav-height relative">
-      <div className="h-5/6 w-full grid grid-cols-2 place-items-center px-20 space-y-6">
+      <div
+        className="h-5/6 relative w-full grid grid-cols-2 place-items-center px-20 space-y-6"
+        ref={parallaxRef}
+      >
         <div className="text-left flex flex-col items-start justify-start gap-6">
-          <h1 id="title" className="hero-title translate-y-40 text-3xl md:text-[4rem] ">
+          <h1
+            id="title"
+            className="hero-title translate-y-40 text-3xl md:text-[4rem] "
+          >
             Ahmed Charfeddine
           </h1>
           <p id="desc" className="translate-y-40 opacity-0">
-            A seasoned <span className="text-primary text-sm md:text-xl ">Frontend Developer</span>{" "}
+            A seasoned{" "}
+            <span className="text-primary text-sm md:text-xl ">
+              Frontend Developer
+            </span>{" "}
             transforming ideas into stunning digital experiences. Let&apos;s
             create something amazing!{" "}
           </p>
         </div>
-        <div></div>
+        <BackgroundCircles parallaxRef={parallaxRef} />
       </div>
     </section>
   );
