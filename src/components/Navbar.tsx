00cf384e46cf1d30b8dffe61224a5040628757f9
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-24 09:14:47 +0100

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,38 @@
+"use client";
+
 import { logo } from "@/utils";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import Image from "next/image";
 import React from "react";
+import { navLists } from "../constants/index";
 
 const Navbar = () => {
+  useGSAP(() => {
+    gsap.to("#logo", { opacity: 1, y: 0 });
+    gsap.to(".sectionLink", { opacity: 1, x: 0, stagger: 0.1, delay: 0.5 });
+  }, []);
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav className="flex w-full screen-max-width">
-        <Image src={logo} alt="AC" width={34} height={34} />
+        <Image
+          id="logo"
+          src={logo}
+          alt="AC"
+          width={34}
+          height={34}
+          className="opacity-0 -translate-y-6"
+        />
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((nav) => (
+            <div
+              key={nav}
+              className="sectionLink px-5 text-sm cursor-pointer translate-x-40 opacity-0 text-gray hover:text-primary transition-all"
+            >
+              {nav}
+            </div>
+          ))}
+        </div>
       </nav>
     </header>
   );
---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export const navLists = ["Home", "About Me", "Skills", "Projects"];
