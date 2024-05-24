78bdc5ad17f024c1e99f504bbe3761e3f1e5372f
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-24 10:09:35 +0100

    init

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     gsap.to(".sectionLink", { opacity: 1, x: 0, stagger: 0.1, delay: 0.5 });
   }, []);
   return (
-    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center z-50">
       <nav className="flex w-full screen-max-width">
         <Image
           id="logo"
