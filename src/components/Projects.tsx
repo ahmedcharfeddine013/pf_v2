4eb22090ed3d20597ce753c0153e2479a3c26463
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-25 01:59:44 +0100

    projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,7 @@
 import { projectsLiist } from "@/constants";
 import { animateWithGsap } from "@/utils/animations";
 import { useGSAP } from "@gsap/react";
+import { Link } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 import { div } from "three/examples/jsm/nodes/Nodes.js";
@@ -12,18 +13,42 @@ const Projects = () => {
       { x: 0, opacity: 1, ease: "power2.inOut", duration: 1, delay: 0.2 },
       {}
     );
-    // animateWithGsap(
-    //   ".skill_cls",
-    //   {
-    //     y: 0,
-    //     opacity: 1,
-    //     ease: "power2.inOut",
-    //     duration: 1,
-    //     delay: 0.2,
-    //     stagger: 0.1,
-    //   },
-    //   {}
-    // );
+    animateWithGsap(
+      ".project-img",
+      {
+        y: 0,
+        opacity: 1,
+        ease: "power2.inOut",
+        duration: 1,
+        delay: 0.2,
+        stagger: 0.1,
+      },
+      {}
+    );
+    animateWithGsap(
+      ".project-title",
+      {
+        y: 0,
+        opacity: 1,
+        ease: "power2.inOut",
+        duration: 1,
+        delay: 0.2,
+        stagger: 0.1,
+      },
+      {}
+    );
+    animateWithGsap(
+      ".project-link",
+      {
+        y: 0,
+        opacity: 1,
+        ease: "power2.inOut",
+        duration: 1,
+        delay: 0.2,
+        stagger: 0.1,
+      },
+      {}
+    );
   }, []);
   return (
     <section className="flex items-center justify-center flex-col gap-10 py-10 relative about-me-gradient z-0">
@@ -33,30 +58,32 @@ const Projects = () => {
       >
         Projects
       </h1>
-      <div className="flex flex-col items-center justify-center gap-10">
+      <div className="w-[80%] p-6 flex justify-center flex-col flex-wrap gap-10 items-center rounded-lg outline-none z-[10] ">
         {projectsLiist.map((project) => (
           <div
             key={project.id}
-            className="flex flex-col gap-5 text-center md:grid md:grid-cols-3 md:w-[80%] md:place-items-center lg:w-[80%] md:text-left md:items-start items-center justify-center "
+            className="flex flex-col items-center justify-center gap-5"
           >
-            <div>
+            <div className="flex items-center justify-center rounded-xl overflow-hidden border-2 border-primary opacity-0 translate-y-20 project-img ">
               <Image
                 src={project.imagePath}
                 alt=""
                 width={500}
                 height={500}
-                className="object-cover"
+                objectFit="cover"
               />
             </div>
-            <div className="col-span-2">
-              <h1>{project.name}</h1>
-              <p>{project.description}</p>
-              <a href={project.githubLink}>{project.githubLink}</a>
+            <div className="flex items-center gap-4 ">
+              <h1 className="text-xl opacity-0 translate-y-20 project-title">
+                {project.name}
+              </h1>
+              <a href={project.githubLink} className="project-link translate-y-20 opacity-0">
+                <Link />
+              </a>
             </div>
           </div>
         ))}
       </div>
-      <div className="w-[80%] p-6 flex justify-center flex-row flex-wrap gap-3 items-center rounded-lg outline-none z-[10] "></div>
       <div className="bg-gradient-to-t from-background to-transparent absolute bottom-0 w-full h-[40%]  "></div>
       <div className="bg-gradient-to-b from-background to-transparent absolute top-0 w-full h-[40%]"></div>
     </section>
